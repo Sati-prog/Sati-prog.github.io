7ccaf61c990b52fbb656d54e10e3535815f523aa
Sati <s.urkumbayeva@mail.ru>
2023-08-05 01:59:14 +0600

    Added react app and php mailer

--- js/script.js
+++ js/script.js
@@ -22,7 +22,35 @@ window.addEventListener('DOMContentLoaded', () => {
         rectangles[i].style.width = item.innerHTML;
     });
     
+});
 
-    // Modal
+//Modal
+
+$('[data-modal=contacts]').on('click', function() {
+    $('.overlay, #thanks').fadeIn('slow');
+});
+
+$('.modal__close').on('click', function() {
+    $('.overlay, #thanks').fadeOut('slow');
+});
+
+// Sending data by mail
+
+$('form').submit(function(e) {
+
+    e.preventDefault();
     
+    $.ajax({
+
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        
+        $(this).find("input").val("");
+        $('input').val('');
+        $('.overlay, #thanks').fadeIn('slow');
+        $("form#form").trigger('reset');
+    });
+    return false;
 });
